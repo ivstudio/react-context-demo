3fb364b62f9ba0411e6f1c25a874c1b65ca1790b
Ignacio Villamar <iggy@ivstudio.com>
2020-09-19 17:40:45 -0400

    Simplify  generate id function

--- src/reducer/todoReducer.js
+++ src/reducer/todoReducer.js
@@ -1,9 +1,4 @@
-const generateId = () => {
-  return (
-    Math.random().toString(36).substring(2, 15) +
-    Math.random().toString(36).substring(2, 15)
-  );
-};
+const generateId = () => Math.floor(Math.random() * 100);
 
 export function todoReducer(state, action) {
   const { payload, type } = action;
